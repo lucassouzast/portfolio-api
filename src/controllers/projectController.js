fe138f960e8bb04c7974fce99d7318b847c8f991
Lucas Pereira de Souza <lucassouza6932@gmail.com>
2026-02-13 13:01:44 -0300

    feat: add user authentication, project management, and update routes

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -41,9 +41,53 @@ const getProjects = async (req, res) => {
     }
 };
 
+const updateProject = async (req, res) => {
+    const { id } = req.params;
+    const { title, description, image, githubLink, liveUrl, technologies, type } = req.body;
+
+    try {
+        const project = await Project.findById(id);
+
+        if (!project) {
+            return res.status(404).json({ message: 'Projeto não encontrado' });
+        }
+
+        if (title !== undefined ) project.title = title;
+        if (description !== undefined) project.description = description;
+        if (image !== undefined ) project.image = image;
+        if (type !== undefined ) project.type = type;
+
+        if (githubLink !== undefined ) project.githubLink = githubLink;
+        if (liveUrl !== undefined ) project.liveUrl = liveUrl;
+        if (technologies !== undefined ) project.technologies = technologies;
+
+        await project.save();
+        res.status(200).json(project);
+    } catch (error) {
+        res.status(500).json({ message: 'Erro ao editar o projeto', error: error.message });
+    }
+};
+
+const deleteProject = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const deletedProject = await Project.findByIdAndDelete(id);
+
+        if (!deletedProject) {
+            return res.status(404).json({ message: 'Projeto não encontrado' });
+        }
+        res.status(200).json({ message: 'Projeto deletado com sucesso' });
+    } catch (error) {
+        res.status(500).json({ message: 'Erro ao deletar o projeto', error: error.message });
+    }
+};
+
 module.exports = {
     createProject,
-    getProjects
+    getProjects,
+    updateProject,
+    deleteProject
 };
 
 
