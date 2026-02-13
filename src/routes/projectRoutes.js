5b582119cbc13e59e4552aa984511dd6bfc95ccc
Lucas Pereira de Souza <lucassouza6932@gmail.com>
2026-02-13 13:02:27 -0300

    feat: enhance project routes with update and delete functionality, and add authentication middleware

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createProject, getProjects } = require('../controllers/projectController');
-
-router.post('/', createProject);
+const { createProject, getProjects, updateProject, deleteProject } = require('../controllers/projectController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.get('/', getProjects);
+
+router.post('/', authMiddleware, createProject);
+router.put('/:id', authMiddleware, updateProject)
+router.delete('/:id', authMiddleware, deleteProject)
 
 module.exports = router;
