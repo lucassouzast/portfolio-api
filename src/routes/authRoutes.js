efe474eb6f36a7c798558410ac45a8951863ad00
Lucas Pereira de Souza <lucassouza6932@gmail.com>
2026-02-13 13:02:18 -0300

    feat: update login route to use user model and bcrypt for authentication

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,19 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 const JWT_SECRET = process.env.JWT_SECRET;
-const ADMIN_USER = process.env.ADMIN_USER;
-const ADMIN_PASS = process.env.ADMIN_PASS;
 
-router.post('/login', (req, res) => {
-  const { username, password } = req.body;
 
-  if (username === ADMIN_USER && password === ADMIN_PASS) {
-    const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: '2h' });
-    return res.json({ token });
-  } else {
-    return res.status(401).json({ message: 'Usuário ou senha incorretos' });
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Usuário ou senha incorretos' });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Usuário ou senha incorretos' });
+    }
+
+    if(!JWT_SECRET) {
+      return res.status(500).json({ message: 'JWT NAO DEFINIDO' });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      JWT_SECRET,
+      { expiresIn: '2h' }
+    )
+    res.json({ token });
+  }  catch (err) {
+    res.status(500).json({ message: 'Erro no servidor' });
   }
 });
 
